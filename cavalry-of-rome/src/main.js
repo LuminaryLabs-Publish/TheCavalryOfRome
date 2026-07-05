544c00d946bf1f79dd7eef5ab267d7ce642bf983
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-05 17:40:08 -0400

    Route app through water safety renderer

--- cavalry-of-rome/src/main.js
+++ cavalry-of-rome/src/main.js
@@ -2,7 +2,7 @@ import { createRealtimeGame } from "./runtime.js";
 import { createCavalryOfRomeKit } from "./cavalry-of-rome-kit.js";
 import { cavalryOfRomeLevel01 } from "./level-01.js";
 import { createInputAdapter } from "./input-adapter.js";
-import { createRenderer } from "./renderer.js";
+import { createRenderer } from "./water-safe-renderer.js";
 
 const canvas = document.querySelector("#game");
 const errorPanel = document.querySelector("#errorPanel");
